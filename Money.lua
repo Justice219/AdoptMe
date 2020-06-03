bcf378a22856336d141d26f847b15346db0fa2e5
Justice219 <65798268+Justice219@users.noreply.github.com>
2020-06-03 13:23:43 -0700

    Add files via upload

---
+++ Money.lua
@@ -0,0 +1,96 @@
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Info"; -- the title (ofc)
+Text = "This Script was created by Justice#6770 and sins#6770"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Money"; -- the title (ofc)
+Text = "You will lag for a bit in just a few seconds. Have fun!"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+
+wait(5)
+
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Started"; -- the title (ofc)
+Text = "THE MONEY HAS STARTED COMING!"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+
+wait(1)
+
+game:GetService("ReplicatedStorage").Remotes.Emote:InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = 3197457149,
+}
+
+game:GetService("ReplicatedStorage").Remotes.Emote:InvokeServer(unpack(args))
+
+
+for count = 1,100 do
+    for count = 1, 100 do
+        game:GetService("ReplicatedStorage").Remotes.Money:FireServer()
+    end
+end
+
+wait(10)
+
+for count = 1,100 do
+    for count = 1, 100 do
+        game:GetService("ReplicatedStorage").Remotes.Money:FireServer()
+    end
+end
+
+wait(10)
+
+for count = 1,100 do
+    for count = 1, 100 do
+        game:GetService("ReplicatedStorage").Remotes.Money:FireServer()
+    end
+end
+
+wait(10)
+
+for count = 1,100 do
+    for count = 1, 100 do
+        game:GetService("ReplicatedStorage").Remotes.Money:FireServer()
+    end
+end
+
+wait(10)
+
+for count = 1,100 do
+    for count = 1, 100 do
+        game:GetService("ReplicatedStorage").Remotes.Money:FireServer()
+    end
+end
+
+for count = 1,100000000 do
+    local args = {
+        [1] = "Internet Protocol",
+    }
+    
+    wait(0.1)
+    
+    local args = {
+        [1] = "This game is bad",
+    }
+    
+    wait(0.1)
+    
+    local args = {
+        [1] = "Internet Protocol",
+    }
+    
+    
+    game:GetService("ReplicatedStorage").Remotes.CustomName:InvokeServer(unpack(args))
+    end
